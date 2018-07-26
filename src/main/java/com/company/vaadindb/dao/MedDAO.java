edb0ac9c5d0e2eb09e50f2b863017f985d28afec
ErykK5 <erykkosiba@onet.eu>
2018-07-26 14:48:25 +0200

    update

--- src/main/java/com/company/vaadindb/dao/MedDAO.java
+++ src/main/java/com/company/vaadindb/dao/MedDAO.java
@@ -1,14 +1,11 @@
 package com.company.vaadindb.dao;
 
 import com.company.vaadindb.model.Med;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public interface MedDAO extends JpaRepository<Med, Long> {
-
-    //List<Med> findByLastNameStartsWithIgnoreCase(String lastName );
 
     public Med findByName(String name);
 
